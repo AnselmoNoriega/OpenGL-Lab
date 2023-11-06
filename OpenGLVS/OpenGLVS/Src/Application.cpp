950ed567103c11e43796c8611c0e99d811e8b416
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-11-05 20:40:55 -0800

    Vertex arrey

--- OpenGLVS/OpenGLVS/Src/Application.cpp
+++ OpenGLVS/OpenGLVS/Src/Application.cpp
@@ -112,6 +112,10 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	window = glfwCreateWindow(640, 480, "Graph Window", NULL, NULL);
 	if (!window)
 	{
@@ -128,7 +132,6 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	const int MAXVERTSIZE = 6;
 	float vertsPos[] = {
 		-0.5f, -0.5f,
 		 0.5f, -0.5f,
@@ -141,10 +144,14 @@ int main(void)
 		2, 3, 0
 	};
 
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, MAXVERTSIZE * 2 * sizeof(float), vertsPos, GL_STATIC_DRAW));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), vertsPos, GL_STATIC_DRAW));
 
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
@@ -152,7 +159,7 @@ int main(void)
 	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, MAXVERTSIZE * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
 	ShaderProgramSource source = ParseShader("Res/Shaders/Basic.shader");
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -162,6 +169,11 @@ int main(void)
 	ASSERT(location != 1);
 	GLCall(glUniform4f(location, 1.0f, 0.0f, 0.0f, 1.0f));
 
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
 	float r = 1.0f;
 	float g = 1.0f;
 	float b = 1.0f;
@@ -170,7 +182,12 @@ int main(void)
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(location, r, g, b, 1.0f));
+
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		r += r + 0.2f <= 1.0f ? 0.2 : -0.8f;
