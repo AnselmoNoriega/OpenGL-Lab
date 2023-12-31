81b0ca35f953d68de57d42d7e95fba7983b6bcbc
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-12-30 19:43:55 -0600

    Creating window

--- OpenGLVS/OpenGL-TWO/Main.cpp
+++ OpenGLVS/OpenGL-TWO/Main.cpp
@@ -4,5 +4,33 @@
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	std::pair<const unsigned int, const unsigned int> winSize(800, 800);
+	GLFWwindow* window = glfwCreateWindow(winSize.first, winSize.second, "Task Two", NULL, NULL);
+	if (window == nullptr)
+	{
+		std::cout << "Window Failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, winSize.first, winSize.second);
+	glClearColor(0.1f, 0.1f, 0.4f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
