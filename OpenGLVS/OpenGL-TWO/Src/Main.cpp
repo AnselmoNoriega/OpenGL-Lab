90d2b49ae21203c54d458bfb7fc7f8864284dd41
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-12-31 18:15:54 -0600

    Index buffer

--- OpenGLVS/OpenGL-TWO/Src/Main.cpp
+++ OpenGLVS/OpenGL-TWO/Src/Main.cpp
@@ -26,9 +26,20 @@ int main()
 
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f,
+
+		 0.0f, -0.5f, 0.0f,
+		-0.25f, 0.0f, 0.0f,
+		 0.25f, 0.0f, 0.0f,
+	};
+
+	GLuint indices[] =
+	{
+		0, 3, 4,
+		1, 3, 5,
+		2, 4, 5
 	};
 
 	std::pair<const unsigned int, const unsigned int> winSize(800, 800);
@@ -61,32 +72,39 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	GLuint VAO, VBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	GLuint VA, VB, EB;
+	glGenVertexArrays(1, &VA);
+	glGenBuffers(1, &VB);
+	glGenBuffers(1, &EB);
+
+	glBindVertexArray(VA);
+	glBindBuffer(GL_ARRAY_BUFFER, VB);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EB);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VA);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &VA);
+	glDeleteBuffers(1, &VB);
+	glDeleteBuffers(1, &EB);
 	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
 	glfwTerminate();
